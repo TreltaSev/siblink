867e0bccf7431c86dfa8ef0fc4053902ec22782c
TreltaSev <treltasev@gmail.com>
2024-02-09 23:36:51 -0500

    Update/ Command: run | Changed conditional statements, redid concat logic

--- src/siblink/commands/run.py
+++ src/siblink/commands/run.py
@@ -24,27 +24,28 @@ def run(location: str, args):
     return
 
   gathered_python: str = Config.python_exe.absolute() if Config.venv.exists() else "python"
-  command = f"set PYTHONPATH=%PYTHONPATH%;{Config.root.absolute()} & {gathered_python} -B "
+  command = f"set PYTHONPATH=%PYTHONPATH%;{Config.root.absolute()} & {gathered_python} -B"
 
   # Append location of file
   if location.is_file():
-    command += str(location.absolute())
+    command = ' '.join(command, str(location.absolute()))
 
   # Check if Directory search for main.py file in dir
   if location.is_dir():
-    normal_file = location.glob("**/main.py")
-    magic_file = location.glob("**/__main__.py")
+    normal_file = next(location.glob("**/main.py"), None)
+    magic_file = next(location.glob("**/__main__.py"), None)
 
-    if magic_file:
-      command += location.absolute()
-    if normal_file:
-      command += str(normal_file[0])
+    if magic_file is not None:
+      command = ' '.join([command, str(location.absolute())])
+    if normal_file is not None:
+      command = ' '.join([command, str(normal_file)])
     else:
       console.error(f"Inputted directory does not contain a main.py or __main__.py file, If \"{location}\" is the name of a registered script, please add the -s or --script flag to this command.")
       return
 
   # Add Extra Arguments to Command if present
-  command += f" {' '.join(args)}"
+  if args:
+    command = ' '.join([command, ' '.join(args)])
 
   # Notify
   console.info(f"[run] Running Command: \"{command}\"")

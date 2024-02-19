4ec9ce1b31d31146d2c111c673b9d2800d749825
TreltaSev <treltasev@gmail.com>
2024-02-18 23:31:31 -0500

    Removed/ Args functionality in command: run | ?: Too much work, deal with it later if needed

--- src/siblink/commands/run.py
+++ src/siblink/commands/run.py
@@ -7,22 +7,16 @@ from siblink import Config, Command
 
 @click.command()
 @click.argument("location")
-@click.argument("args", nargs=-1)
 @Config.load_predetermined
-def run(location: str, args):
+@Config.load_config
+def run(location: str):
   """
   Run python scripts and programs with this command by referencing a file or directory which contains a main.py file.
 
   location(str): Path of script or directory to run, if the inputted is a directory, a main.py file or __main__.py file is checked for within the said directory.
-
-  args(any): Any value past the location string will be added to the end of the compiled command. Limited to anything that doesn't contain ";" or starts with "-" or "--"
   """
 
   command = Command.RunScaffold(location).generate()
-
-  # Add Extra Arguments to Command if present
-  if args:
-    command = ' '.join([command, ' '.join(args)])
 
   # Notify
   console.info(f"[run] Running Command: \"{command}\"")

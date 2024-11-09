c90aa3123ddb20a82692aa5d81eb7b49af252484
TreltaSev <treltasev@gmail.com>
2024-11-09 15:57:50 -0500

    Update/ Debug Checkg

--- src/siblink/commands/run.py
+++ src/siblink/commands/run.py
@@ -29,7 +29,6 @@ def run(location: str, ps: bool, args: list):
   command = Command.RunScaffold(location, ps, args).generate()
 
   # Notify
-  print(f"::: {Config.os} ::: {Config.__dict__}")
   console.info(f"[run] Running Command: \"{command}\"")
 
   # Run Command
